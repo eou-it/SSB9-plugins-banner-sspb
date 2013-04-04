1e7fcef00d6fa52be2a3a58fccbc6281671b18e4
jzhong <jason.zhong@ellucian.com>
2013-04-04 07:53:18 -0400

    Initial creation of project

---
+++ grails-app/views/index.gsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jzhong
+  Date: 4/4/13
+  Time: 6:54 AM
+  To change this template use File | Settings | File Templates.
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+  <title>Test Page</title>
+</head>
+<body>
+Test Page
+</body>
+</html>
---
+++ test/unit/net/hedtech/banner/sspb/CompileServiceTest.groovy
@@ -0,0 +1,68 @@
+package net.hedtech.banner.sspb
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jzhong
+ * Date: 4/1/13
+ * Time: 3:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class CompileServiceTest extends GroovyTestCase  {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+
+    void testCompileAll() {
+        // test all page model compilation
+        def maxModelId = 6
+        def modelPath = "C:/projects/grails_apps/banner-sspb/test/testData/model/PageModel"
+        def getModelPath = { id -> return modelPath + id + '.json' }
+
+        def pagePath = "C:/projects/grails_apps/banner-sspb/target/compiledPage/page"
+        def getPagePath = { id -> return pagePath + id + '.html' }
+
+        for (i in (1..maxModelId)) {
+            def modelFilePath = getModelPath(i)
+            def pageFilePath = getPagePath(i)
+
+            println "testing $modelFilePath}"
+
+            def pageSource = new File(modelFilePath).getText()
+
+            // parse, normalize and validate page model
+            def validateResult =  CompileService.preparePage(pageSource)
+            if (validateResult.valid) {
+                // generate JS first because it will set the binding correctly for some components
+                def compiledJSCode=CompileService.compileController(validateResult.pageComponent)
+                //println "Compiled Controller = $compiledJSCode\n"
+                println "JavaScript is compiled"
+                def htmlOutput= new File('TestJS.js')
+                htmlOutput.text = compiledJSCode
+
+                def compiledView = CompileService.compile2page(validateResult.pageComponent)
+                //println "Compiled View = $compiledView\n"
+                println "HTML is compiled"
+                htmlOutput= new File('TestHtml.html')
+                htmlOutput.text = compiledView
+
+                def combinedView = CompileService.assembleFinalPage(compiledView, compiledJSCode)
+                //println "Compbined View = $combinedView\n"
+                println "Page is compiled"
+                htmlOutput= new File(pageFilePath)
+                htmlOutput.text = combinedView
+
+            } else {
+                println "modelFilePath validation error:\n" + validateResult.errors.join('\n')
+                assert (false)
+            }
+
+        }
+        assert(true)
+
+    }
+}
