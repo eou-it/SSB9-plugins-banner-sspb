27059b73ce1a995c6412efbfe4c7730874238b48
Harry van Thor <harry.van.thor@ellucian.com>
2013-04-27 10:41:08 +0200

    resolve merge master conflict

--- grails-app/controllers/net/hedtech/banner/sspb/PageModelComposerController.groovy
+++ grails-app/controllers/net/hedtech/banner/sspb/PageModelComposerController.groovy
@@ -26,26 +26,45 @@ class PageModelComposerController {
         def pageModel
         //println "Page ModelView = " + pageInstance.modelView
         if (params.modelView)  {
-            def validateResult =  compileService.preparePage(params.modelView)
-            if (validateResult.valid) {
-                def compiledJSCode=compileService.compileController(validateResult.pageComponent)
-                statusMessage="JavaScript is compiled\n"
-                def compiledView = compileService.compile2page(validateResult.pageComponent)
-                statusMessage+="HTML is compiled\n"
-                def combinedView = compileService.assembleFinalPage(compiledView, compiledJSCode)
-                //validateHtml(combinedView)
-                println "Page is compiled\n"
-                pageInstance.modelView=params.modelView
-                pageInstance.compiledView = compiledView
-                pageInstance.compiledController=compiledJSCode
-                pageInstance=pageInstance.save()
-                pageModel=[status:statusMessage, pageInstance: pageInstance, modelView: pageInstance.modelView, compiledView: pageInstance.compiledView, compiledController: pageInstance.compiledController]
-            } else {
-                pageModel=[status:statusMessage, pageInstance: pageInstance, modelView: params.modelView, compiledView: "Page model validation error (model not saved):\n" + validateResult.error.join('\n'),
-                        compiledController:""]
-            }
+            if (pageInstance.constantName.endsWith("Ajs"))  {
+				def validateResult =  compileService.preparePage(params.modelView)
+				if (validateResult.valid) {
+					def compiledJSCode=compileService.compileController(validateResult.pageComponent)
+					statusMessage="JavaScript is compiled\n"
+					def compiledView = compileService.compile2page(validateResult.pageComponent)
+					statusMessage+="HTML is compiled\n"
+					def combinedView = compileService.assembleFinalPage(compiledView, compiledJSCode)
+					//validateHtml(combinedView)
+					println "Page is compiled\n"
+					pageInstance.modelView=params.modelView
+					pageInstance.compiledView = compiledView
+					pageInstance.compiledController=compiledJSCode
+					pageInstance=pageInstance.save()
+					pageModel=[status:statusMessage, pageInstance: pageInstance, modelView: pageInstance.modelView, compiledView: pageInstance.compiledView, compiledController: pageInstance.compiledController]
+				} else {
+					pageModel=[status:statusMessage, pageInstance: pageInstance, modelView: params.modelView, compiledView: "Page model validation error (model not saved):\n" + validateResult.error.join('\n'),
+							compiledController:""]
+				}
+            } else {  //Original compiler
+                def validateResult =  compileService.preparePage(pageInstance.modelView)
+                if (validateResult.valid) {
+                    def compiledJSCode=compileService.compileController(validateResult.pageComponent)
+                    statusMessage="JavaScript is compiled\n"
+                    def compiledView = compileService.compile2page(validateResult.pageComponent)
+                    statusMessage+="HTML is compiled\n"
+                    def combinedView = compileService.assembleFinalPage(compiledView, compiledJSCode)
+                    //validateHtml(combinedView)
+                    println "Page is compiled\n"
+                    pageInstance.compiledView = compiledView
+                    pageInstance.compiledController=compiledJSCode
+					pageInstance=pageInstance.save()
+					pageModel=[status:statusMessage, pageInstance: pageInstance, modelView: pageInstance.modelView, compiledView: pageInstance.compiledView, compiledController: pageInstance.compiledController]
 
-
+                } else {
+					pageModel=[status:statusMessage, pageInstance: pageInstance, modelView: params.modelView, compiledView: "Page model validation error (model not saved):\n" + validateResult.error.join('\n'),
+							compiledController:""]
+                }            
+			}
         }
         render (view:"composer", model: [pageModel: pageModel])
         println "finished compile on ${new Date()}"
