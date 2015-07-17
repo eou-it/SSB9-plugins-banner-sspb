1d0713f67806f307cf9f40ffe1fd832aa400c061
Harry van Thor <harry.van.thor@ellucian.com>
2015-07-17 11:45:17 +0200

    grails2.3.7 upgrade working

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,14 +1,14 @@
+grails.servlet.version = "2.5"
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
-//grails.plugin.location.'restful-api'="../restful-api.git"
-grails.plugin.location.'banner_ui_ss'="../banner_ui_ss.git"
+//grails.plugin.location.'banner_ui_ss'="../banner_ui_ss.git"
 grails.plugin.location.'banner-core'="../banner_core.git"
-grails.plugin.location.'banner-codenarc'="../banner_codenarc.git"
-grails.plugin.location.'sghe-aurora'="../sghe_aurora.git"
-grails.plugin.location.'spring-security-cas'="../spring_security_cas.git"
-grails.plugin.location.'i18n_core'="../i18n_core.git"
+//grails.plugin.location.'banner-codenarc'="../banner_codenarc.git"
+//grails.plugin.location.'sghe-aurora'="../sghe_aurora.git"
+//grails.plugin.location.'spring-security-cas'="../spring_security_cas.git"
+//grails.plugin.location.'i18n_core'="../i18n_core.git"
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
@@ -19,27 +19,31 @@ grails.project.dependency.resolution = {
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
     repositories {
-        inherits true // Whether to inherit repository definitions from plugins
-
-        grailsPlugins()
-        grailsHome()
-		grailsCentral()
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        mavenLocal()
-        mavenCentral()
-        mavenRepo "https://repo.grails.org/grails/plugins"
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        if (System.properties['PROXY_SERVER_NAME']) {
+            mavenRepo "${System.properties['PROXY_SERVER_NAME']}"
+        } else
+        {
+			grailsPlugins()
+			grailsHome()
+			grailsCentral()
+			// uncomment the below to enable remote dependency resolution
+			// from public Maven repositories
+			mavenLocal()
+			mavenCentral()
+			mavenRepo "https://repo.grails.org/grails/plugins"
+			//mavenRepo "http://snapshots.repository.codehaus.org"
+			//mavenRepo "http://repository.codehaus.org"
+			//mavenRepo "http://download.java.net/maven/2/"
+			//mavenRepo "http://repository.jboss.com/maven2/"
+		}
     }
     dependencies {
+///*
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.21'
         test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
-		
+/*
 		// HvT: resolve compilation issue with Banner-core
         // taken from grails-spring-security-cas / grails-app / conf / BuildConfig.groovy
         compile('org.springframework.security:spring-security-cas-client:3.0.7.RELEASE') {
@@ -51,6 +55,7 @@ grails.project.dependency.resolution = {
             excludes 'xmlsec', 'opensaml', 'spring-beans', 'spring-test', 'spring-core',
                     'spring-context', 'log4j', 'junit', 'commons-logging', 'servlet-api'
         }
+*/
     }
 
     plugins {
@@ -58,48 +63,34 @@ grails.project.dependency.resolution = {
         compile ":spring-security-core:1.2.7.3"
         compile ":webxml:1.4.1"
         compile ':cache-headers:1.1.5'
-        compile ':codenarc:0.18.1'
-        compile ':csv:0.3.1'
-        compile ':feeds:1.5'
+		compile ":hibernate:3.6.10.10"
+		compile ":tomcat:7.0.52.1"
+		compile ":functional-test:2.0.0"
+		//compile ':codenarc:0.21'
+        //compile ':csv:0.3.1'
+        //compile ':feeds:1.5'
 
-        compile ':selenium:0.8'
-        compile ':selenium-rc:1.0.2'
-        compile  ":inflector:0.2"
+        //compile ':selenium:0.8'
+        //compile ':selenium-rc:1.0.2'
+        //compile  ":inflector:0.2"
         compile ':restful-api:1.0.0'
-
-        // compatibility?
-        //compile ":tomcat:1.3.7"
-        //compile ":hibernate:1.3.7"
-
-        runtime ":hibernate:$grailsVersion"
-        //runtime ":jquery:1.8.2"
-        //runtime ":jquery-ui:1.8.24"
-        //compile ":angularjs-resources:1.0.2"
-        //runtime ":resources:1.1.6"
-        
 		
-		build(":tomcat:$grailsVersion",
+		build(
               ":release:2.2.0",
               ":rest-client-builder:1.0.3") {
             export = false
         }
 
-		compile ":tomcat:$grailsVersion"
-        /*
-		compile (":inflector:0.2"
-		        ,":cache-headers:1.1.5"
-				//,':cache:1.0.0'
-				)
-          */
-        // Fix HvT
-        compile 'org.grails.plugins:codenarc:0.18.1'
-        // Fix  HvT
-        compile ":functional-test:2.0.RC1"
+
+
 
 
         test(":spock:0.7") {
             exclude "spock-grails-support"
         }
-        test ':code-coverage:1.2.5'
+        test ':code-coverage:2.0.3-2',
+		{
+            excludes 'xercesImpl'
+        }
     }
 }
