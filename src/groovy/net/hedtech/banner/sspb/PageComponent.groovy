cf7bb5810af505b20b143d87dca85d9380543fdb
Ron Boukens <Ron.Boukens@ellucian.com>
2015-12-11 16:03:18 +0100

    EXTZ-1039: Fix for Radio button display (defaults to vertical display now; set horizontal display by using 'radio-inline' class for valueStyle).

--- src/groovy/net/hedtech/banner/sspb/PageComponent.groovy
+++ src/groovy/net/hedtech/banner/sspb/PageComponent.groovy
@@ -798,12 +798,13 @@ class PageComponent {
                 def radioLabelStyleStr= """class="pb-${parent.type} pb-item pb-radiolabel $labelStyle" """
 
                 result = """
-                  |<span class="$valueStyle" ng-repeat="$SELECT_ITEM in $arrayName" $initTxt >
-                  | <input ${idAttribute("-radio-$idxStr")} type="radio" ng-model=$ngModel name="$nameTxt" $ngChange value="{{$SELECT_ITEM.$valueKey}}"/>
-                  | <label  ${idAttribute("-label-$idxStr")} ${idForAttribute("-radio-$idxStr")} $radioLabelStyleStr> {{$SELECT_ITEM.$labelKey}} </label>
-                  |</span>""".stripMargin()
+                  |<div class="$valueStyle" ng-repeat="$SELECT_ITEM in $arrayName" $initTxt >
+                  | <label  ${getIdAttr("-label-$idxStr")} ${getIdFor("-radio-$idxStr")} $radioLabelStyleStr>
+                  | <input ${getIdAttr("-radio-$idxStr")} type="radio" ng-model=$ngModel name="$nameTxt" $ngChange value="{{$SELECT_ITEM.$valueKey}}"/>
+                  | {{$SELECT_ITEM.$labelKey}} </label>
+                  |</div>""".stripMargin()
 
-                result = """<span ${idAttribute(idTxtParam)} $autoStyleStr> $result </span>"""
+                result = """<div ${getIdAttr(idTxtParam)} $autoStyleStr> $result </div>"""
                 break;
             case COMP_TYPE_LITERAL:
                 //Todo: should we do something for safe/unsafe binding as in next item type?
