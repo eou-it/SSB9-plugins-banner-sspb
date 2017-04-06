443cb9c9b37c3d6b2b481ac6555ef60f65060839
VinayHS <VinayHS@Vinay-HS.local>
2017-04-06 15:52:53 +0530

    EXTZ-1585 Remove dataSource_sspb from Banner Extensibility configuration.

--- ddl/sspbmgr_090100/virtual_domain1.sql
+++ ddl/sspbmgr_090100/virtual_domain1.sql
@@ -1,39 +1,39 @@
 --
 -- virtual_domain1.sql
 --
--- audit trail: pb9.0.0                      init       date
--- 1.                                          hvt      2016-06-08
---    project: pb
---    object:  virtual_domain
---    type:    table
---    description:
---    table with pagebuilder virtual domains
---    purpose:
---    this table stores the pagebuilder virtual domains
---    todo review/correct detailed description and purpose of object
--- audit trail end
+-- AUDIT TRAIL: PB9.0.0                      INIT       DATE
+-- 1.                                          HvT      2016-06-08
+--    PROJECT: PB
+--    OBJECT:  VIRTUAL_DOMAIN
+--    TYPE:    TABLE
+--    DESCRIPTION:
+--    Table with PageBuilder Virtual Domains
+--    PURPOSE:
+--    This table stores the PageBuilder Virtual Domains
+--    TODO Review/Correct Detailed Description and purpose of Object
+-- AUDIT TRAIL END
 --
 
-  create table virtual_domain
+  CREATE TABLE VIRTUAL_DOMAIN
    (
-    service_name                    varchar2(60 char)        not null ,
-    type_of_code                    varchar2(1 char),
-    code_get                        clob                     not null ,
-    code_delete                     clob,
-    code_post                       clob,
-    code_put                        clob,
-    id                              number(19,0)             not null ,
-    version                         number(19,0)             not null ,
-                                    date_created timestamp (6),
-    last_updated timestamp (6),
-    file_timestamp timestamp (6)
+    SERVICE_NAME                    VARCHAR2(60 CHAR)        NOT NULL ,
+    TYPE_OF_CODE                    VARCHAR2(1 CHAR),
+    CODE_GET                        CLOB                     NOT NULL ,
+    CODE_DELETE                     CLOB,
+    CODE_POST                       CLOB,
+    CODE_PUT                        CLOB,
+    ID                              NUMBER(19,0)             NOT NULL ,
+    VERSION                         NUMBER(19,0)             NOT NULL ,
+                                    DATE_CREATED TIMESTAMP (6),
+    LAST_UPDATED TIMESTAMP (6),
+    FILE_TIMESTAMP TIMESTAMP (6)
    )
-  storage (initial     &mmedtab_initial_extent
-           next        &mmedtab_next_extent
-           minextents  &mmedtab_min_extents
-           maxextents  &mmedtab_max_extents
-           pctincrease &mmedtab_pct_increase)
-  tablespace           &mmedtab_tablespace_name
-  pctfree              &mmedtab_pct_free
-  pctused              &mmedtab_pct_used
+  STORAGE (INITIAL     &MMEDTAB_INITIAL_EXTENT
+           NEXT        &MMEDTAB_NEXT_EXTENT
+           MINEXTENTS  &MMEDTAB_MIN_EXTENTS
+           MAXEXTENTS  &MMEDTAB_MAX_EXTENTS
+           PCTINCREASE &MMEDTAB_PCT_INCREASE)
+  TABLESPACE           &MMEDTAB_TABLESPACE_NAME
+  PCTFREE              &MMEDTAB_PCT_FREE
+  PCTUSED              &MMEDTAB_PCT_USED
 ;
