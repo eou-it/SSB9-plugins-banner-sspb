881825d0a4496e88ed2951d8c29672c2c7ffc934
gurunath_dk <gurunat.dk@ellucian.com>
2018-11-22 21:39:35 +0530

    NOJIRA: Reverting the version number in sspb plugin

--- BannerSspbGrailsPlugin.groovy
+++ BannerSspbGrailsPlugin.groovy
@@ -16,7 +16,7 @@ import org.springframework.web.context.support.ServletContextResourcePatternReso
 
 class BannerSspbGrailsPlugin {
     // the plugin version
-    def version = "9.2.1"
+    def version = "0.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
     // resources that are excluded from plugin packaging
