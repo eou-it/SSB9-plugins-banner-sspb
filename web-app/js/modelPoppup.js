625825de2969ab7af041d7a805255bee0a7692f6
Venu Thanneeru <venu.thanneeru@ellucian.com>
2019-02-04 13:00:53 +0530

    EXTZ-1741 : PB- Better organization of virtual domains and Pages- Analysis PB- Better organization of virtual domains and Pages- Analysis

--- web-app/js/modelPoppup.js
+++ web-app/js/modelPoppup.js
@@ -6,14 +6,6 @@
     'use strict';
 
     angular.module('modalPopup',['xe-ui-components'])
-        .config(function($httpProvider) {
-            $httpProvider.defaults.headers.common['Cache-Control'] = 'no-cache';
-            $httpProvider.defaults.cache = false;
-            if (!$httpProvider.defaults.headers.get) {
-                $httpProvider.defaults.headers.get = {};
-            }
-            $httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
-        })
         .controller("nameModalPopupCtrl", ["$scope","$timeout","$http", "$q", "$filter",  function($scope, $timeout,$http, $q, $filter) {
             $scope.nocolumnFilterMenu = true;
             $scope.rtl = "xe-ui-components.min";
