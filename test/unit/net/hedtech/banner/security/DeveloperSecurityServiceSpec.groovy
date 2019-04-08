934ce0b7bdbf909da4fcf550f410bd2d69d8a5f9
gurunath_dk <gurunat.dk@ellucian.com>
2019-04-08 17:03:00 +0530

    EXTZ-3242: Updating copyright

--- test/unit/net/hedtech/banner/security/DeveloperSecurityServiceSpec.groovy
+++ test/unit/net/hedtech/banner/security/DeveloperSecurityServiceSpec.groovy
@@ -1,3 +1,6 @@
+/*******************************************************************************
+ Copyright 2019 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
 package net.hedtech.banner.security
 
 import grails.test.mixin.TestFor
