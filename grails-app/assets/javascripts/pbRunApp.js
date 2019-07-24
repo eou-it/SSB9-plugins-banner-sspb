403d40dcd9a16fee416682834d56b5359980e8a4
Vinay H S <vinay.hs@ellucian.com>
2019-07-24 18:03:57 +0530

    EXTZ-3553 Fix issues in weblogic.

--- grails-app/assets/javascripts/pbRunApp.js
+++ grails-app/assets/javascripts/pbRunApp.js
@@ -32,6 +32,10 @@ alert = function(message, params ){ //message,promptMessage,type,flash,prompts) 
 //Temporary fix for framework issue. ToDo remove when not needed any more.
 $(function() {
     _.defer( ContentManager.setContentPosition );
+    //Fix for WebLogic trailing slash issue
+    if($('#branding').attr('href') ==='/BannerExtensibility') {
+        $('#branding').attr('href', '/BannerExtensibility/')
+    }
 });
 
 //remove by value for arrays. Return if value was remove
